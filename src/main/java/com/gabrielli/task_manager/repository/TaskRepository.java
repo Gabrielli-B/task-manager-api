fe3a041857ce24f272f00bcd726731ce17082411
Gabrielli <gabrielliborba42@gmail.com>
2026-01-20 10:23:03 -0300

    Criação do repository da entidade Task

---
+++ src/main/java/com/gabrielli/task_manager/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.gabrielli.task_manager.repository;
+
+import com.gabrielli.task_manager.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository  extends JpaRepository<Task,Long> {
+}
