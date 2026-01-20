110ebca86ecdc09db46ac06bef4070aa58dd2b08
Gabrielli <gabrielliborba42@gmail.com>
2026-01-20 10:27:15 -0300

    Configuração de injeção de dependência no Controller e Service

--- src/main/java/com/gabrielli/task_manager/controller/TaskController.java
+++ src/main/java/com/gabrielli/task_manager/controller/TaskController.java
@@ -1,10 +1,18 @@
 package com.gabrielli.task_manager.controller;
 
+import com.gabrielli.task_manager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/taskManagerApi")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
 }
--- src/main/java/com/gabrielli/task_manager/service/TaskService.java
+++ src/main/java/com/gabrielli/task_manager/service/TaskService.java
@@ -1,8 +1,14 @@
 package com.gabrielli.task_manager.service;
 
+import com.gabrielli.task_manager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
 
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 }
