c38b7e709365f106457e757a13b8c785675e6d40
Gabrielli <gabrielliborba42@gmail.com>
2026-01-22 08:19:22 -0300

    Adicionando métodos de criação,listagem e remoção de tarefas no TaskService

--- src/main/java/com/gabrielli/task_manager/service/TaskService.java
+++ src/main/java/com/gabrielli/task_manager/service/TaskService.java
@@ -1,7 +1,10 @@
 package com.gabrielli.task_manager.service;
 
+import com.gabrielli.task_manager.model.Task;
 import com.gabrielli.task_manager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -11,4 +14,18 @@ public class TaskService {
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
+
+    //listar tarefas
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+    //criar
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+    //deletar
+    public void deleteTask(Long id){
+        taskRepository.deleteById(id);
+    }
+
 }
