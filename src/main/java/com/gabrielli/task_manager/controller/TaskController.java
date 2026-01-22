6bd5d67bb5bd527680d5c1941ae0589f27919132
Gabrielli <gabrielliborba42@gmail.com>
2026-01-22 08:26:08 -0300

    Implementando endpoints de criação, listagem e exclusão de tarefas no controller

--- src/main/java/com/gabrielli/task_manager/controller/TaskController.java
+++ src/main/java/com/gabrielli/task_manager/controller/TaskController.java
@@ -1,9 +1,11 @@
 package com.gabrielli.task_manager.controller;
 
+import com.gabrielli.task_manager.model.Task;
 import com.gabrielli.task_manager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/taskManagerApi")
@@ -15,4 +17,12 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @GetMapping
+    public List<Task> getAllTasks(){return taskService.getAllTasks();}
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){return taskService.createTask(task);}
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){taskService.deleteTask(id);}
 }
